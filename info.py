ea17c5f28bcb1e3593554ac1854669d2071aa4a2
Nikhil792 <114512871+Nikhil792@users.noreply.github.com>
2025-08-23 22:45:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -166,12 +166,12 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://weekly-kippie-technik659-053cab2f.koyeb.app/'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'weekly-kippie-technik659-053cab2f.koyeb.app/'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else "https://{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
-SESSION_NAME = str(environ.get('SESSION_NAME', 'Trexowner'))
+SESSION_NAME = str(environ.get('SESSION_NAME', 'Trexofficialsbot'))
 MULTI_CLIENT = False
 name = str(environ.get('name', 'trexowner'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
