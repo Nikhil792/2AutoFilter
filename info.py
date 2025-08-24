5e0bf0db451c408ba44d281139fa9caae0313bd6
Nikhil792 <114512871+Nikhil792@users.noreply.github.com>
2025-08-24 09:58:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,7 +69,7 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://GG:GG@cluster0.nax1bxf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "GGG")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Lucy_files')
 
 # ============================
