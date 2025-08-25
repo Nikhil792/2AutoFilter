cce2c6562323f8306eb8e03b5b968450e1bd2f76
Nikhil792 <114512871+Nikhil792@users.noreply.github.com>
2025-08-25 16:51:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -102,7 +102,7 @@ IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+14pd-6uyvbA2Y2Q1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/trexofficialhub')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Trexowner')
-MOVIE_UPDATE_CHANNEL_LNK = environ.get('MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/+I4IGJI4RuCM5M2E1')
+MOVIE_UPDATE_CHANNEL_LNK = environ.get('MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/+OXOBPSHFpQhjZWNl')
 OWNERID = int(os.environ.get('OWNERID', '6667067260'))  # Replace with the actual admin ID
 
 # ============================
